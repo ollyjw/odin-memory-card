bdc8cb6ec4292fd0dd44154f10d5ebdaba91c7f6
ollyjw <owilcocks@gmail.com>
2024-02-26 16:34:57 +0000

    fetch & render 12 random characters on pageload

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,18 +1,33 @@
 import { useState, useEffect } from "react";
 import CardsContainer from "./CardsContainer";
-import { fetchCharacters } from "../helpers/fetchCharacters";
+import { fetchCharactersById } from "../helpers/fetchCharacters";
 
 export default function Game() {
-    const [characterData, setCharacterData] = useState([]);
+    const [characterData, setCharacterData] = useState([]);      
 
     useEffect(() => {
-        async function fetchData() {
-            const aotCharacterObjs = await fetchCharacters();
-            setCharacterData(aotCharacterObjs.results);
+
+        async function fetchTargetData() {
+            const aotCharIdArr = [188, 2, 1, 5, 12, 8, 86, 3, 4, 10, 87, 67, 95, 101, 124, 74, 184, 104, 57, 193, 88, 90, 89, 82, 91, 92, 182, 71, 160];
+            const newArr = [];
+
+            // Take 12 random items from id array, push them to new array and remove each item from original array with splice to avoid repetitions
+            for (let i = 0; i < 12; i++) {
+                const randomIndexNo = Math.floor(Math.random() * aotCharIdArr.length);
+                newArr.push(aotCharIdArr[randomIndexNo]);
+                aotCharIdArr.splice(randomIndexNo,1); // splice(start, deleteCount)
+            }
+
+            console.log(newArr);
+
+            const newUrl = `https://api.attackontitanapi.com/characters/${newArr.toString()}`;
+
+            const aotTargetChars = await fetchCharactersById(newUrl);
+            setCharacterData(aotTargetChars);
         }
 
-        fetchData();
-    
+        fetchTargetData();
+
     }, []);
 
     return (
--- src/helpers/fetchCharacters.jsx
+++ src/helpers/fetchCharacters.jsx
@@ -3,20 +3,19 @@
 async function fetchCharacters() {
   const response = await fetch(`https://api.attackontitanapi.com/characters`, {mode: 'cors'});
   const characterData = await response.json();
-  //console.log(characterData);
-  return characterData;  
+  return characterData;
 }
 
+// You can get multiple characters by adding a list of ids as the request parameter /characters/188,1
+// GET https://api.attackontitanapi.com/characters/188,1"
 
-// "You can get a single character by adding an id as the request parameter /characters/188:
-// GET https://api.attackontitanapi.com/characters/188"
-async function fetchCharacterById(id) {
-  const response = await fetch(`https://api.attackontitanapi.com/characters${id}`, {mode: 'cors'});
+async function fetchCharactersById(url) {
+  const response = await fetch(`${url}`, {mode: 'cors'});
   const characterData = await response.json();
   return characterData;
 }
 
 export {
   fetchCharacters, 
-  fetchCharacterById,
+  fetchCharactersById,
 };
